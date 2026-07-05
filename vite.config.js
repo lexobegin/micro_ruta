f5b903527c2c71b3e871b21f5ef89cc36ef467dd
Alex Orellana <lexorel88@gmail.com>
2026-07-04 21:19:49 -0400

    corrección en producción vercel
    Falto instalar terser

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
 })
 **/
+/** 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -33,5 +34,49 @@ export default defineConfig({
         },
       },
     },
+  },
+})
+*/
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL || 'http://localhost:8000',
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+  output: {
+    manualChunks(id) {
+      if (id.includes('node_modules')) {
+        if (
+          id.includes('react') ||
+          id.includes('react-dom') ||
+          id.includes('react-router-dom')
+        ) {
+          return 'vendor';
+        }
+
+        if (id.includes('leaflet') || id.includes('react-leaflet')) {
+          return 'map';
+        }
+
+        return 'vendor';
+      }
+    }
+  }
+},
   },
 })
